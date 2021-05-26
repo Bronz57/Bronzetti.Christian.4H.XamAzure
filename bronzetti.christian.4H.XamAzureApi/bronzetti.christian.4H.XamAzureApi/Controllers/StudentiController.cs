aa2cf5f9af1950764ca89bb609ab0862e3fcc1f1
Bronz57 <chbronz88@gmail.com>
2021-05-26 09:29:08 +0200

    Primo commit

---
+++ bronzetti.christian.4H.XamAzureApi/bronzetti.christian.4H.XamAzureApi/Controllers/StudentiController.cs
@@ -0,0 +1,27 @@
+﻿using bronzetti.christian._4h.XamAzure.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bronzetti.christian._4H.XamAzureAPI.Controllers
+{   //Decoratore
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentiController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Studente> Get()
+        {
+            List<Studente> studenti = new List<Studente>();
+
+            studenti.Add(new Studente { Nome = "Christian", Cognome = "Bronzetti", Immagine = "christian.bronzetti.jpg" });
+            studenti.Add(new Studente { Nome = "Giammarco", Cognome = "Savini", Immagine = "giammarco.savini.jpg" });
+
+            return studenti;
+        }
+    }
+}
---
+++ bronzetti.christian.4h.XamAzure/bronzetti.christian.4h.XamAzure/bronzetti.christian.4h.XamAzure/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using bronzetti.christian._4h.XamAzure.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace bronzetti.christian._4h.XamAzure
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            List<Studente> elenco = new List<Studente>();
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                
+                string response = await client.GetStringAsync("https://bronzetti-christian-4h-xamazureapi.conveyor.cloud/Studenti");
+                elenco = JsonConvert.DeserializeObject<List<Studente>>(response);
+            }
+            catch (Exception err)
+            {
+                await DisplayAlert("Non va", err.Message, "Ok");
+            }
+
+            
+            lvStudenti.ItemsSource = elenco;
+        }
+    }
+}
